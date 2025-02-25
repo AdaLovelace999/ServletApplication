4e966042c49c86741007403e31d374b9585272b9
Ada Lovelace <99196054+almazzzik11111@users.noreply.github.com>
2025-02-25 16:36:20 +0300

    Delete sout "AAAAAA" and change "MMMMMMMM"

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,8 +2,8 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT DEVELOP");
         System.out.println("LEARNING !");
-        System.out.println("AAAAAAAAA");
-        System.out.println("BBBBBBB");
+
+        System.out.println("MMMMMMMM");
         System.out.println(" !!!!! ");
     }
 }
