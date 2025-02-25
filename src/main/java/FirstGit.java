175e03acd35e7f3d8ae1dd0f2815e65c73a32b52
Ada Lovelace <99196054+almazzzik11111@users.noreply.github.com>
2025-02-25 16:37:39 +0300

    Add sout "AAAAAA sdfghj"

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT DEVELOP");
         System.out.println("LEARNING !");
-        System.out.println("AAAAAAAAA");
+        System.out.println("AAAAAAAAA sdfghj");
         System.out.println("BBBBBBB");
         System.out.println(" !!!!! ");
     }
